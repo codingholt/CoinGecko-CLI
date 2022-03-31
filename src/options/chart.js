180375d74c7262dcf3325ca00c15a00f87bf0d51
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-03-31 11:09:21 +0200

    added chart.js

---
+++ src/options/chart.js
@@ -0,0 +1,14 @@
+import chart from "cli-chart";
+import { client } from "../client";
+
+
+const config = {
+    // xlabel: ,
+    // ylabel: ,
+    // direction: ,
+    // width:,
+    // height: ,
+    // lmargin: ,
+    // step: ,
+};
+const chart = new Chart(config);
