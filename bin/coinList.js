ebd9bc32ba3455bb1bf634b49108075098dde21b
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-03-31 10:06:11 +0200

    added cois.json

--- bin/coinList.js
+++ bin/coinList.js
@@ -6,18 +6,6 @@ import fs from 'fs'
 async function coinList(){
     const coinList = await client.coinList();
 
-    fs.writeFile(
-
-        'coins.json',
-    
-        JSON.stringify(coinList),
-    
-        function (err) {
-            if (err) {
-                console.error('Crap happens');
-            }
-        }
-    );
 
 
 }
--- src/util/responseError.js
+++ src/util/responseError.js
@@ -1,7 +1,12 @@
 function checkErr(response){
     if(response.hasOwnProperty('error')) {
-        console.error(response['error'])
-        return;
+
+        //CALL THE SEARCHFUNC FOR OTHER COINS
+
+
+        // IF SEARCH IS NO RESULT
+        console.error('COIN COULD NOT BE FOUND IN THE DATABASE, PLEASE USE THE API-ID AS PROVIDED ON COINGECKO.COM FOR THE BEST RESULTS')
+        process.exit();
 }}
 
 export {checkErr}
