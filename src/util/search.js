ea09751861bccd3ecff2e1fd0c7d16c9c8114e9e
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-03-31 16:24:47 +0200

    added threshold for search result and err message

--- src/util/search.js
+++ src/util/search.js
@@ -1,4 +1,5 @@
 import Fuse from 'fuse.js'
+
 import { coins } from './coins.js'
 
 async function search(coin){
@@ -11,9 +12,13 @@ async function search(coin){
   const fuse = new Fuse(coins, options)
 
   const result = await fuse.search(coin)
-
+  if(result[0]['score'] > 1){
   coin = result[0]['item']['id']
   return coin
+  }else{
+    console.log('\nCoin not found in database please use the ticker as listed on coingecko. \n\nIf it still doesnt work please open an issue here:\n    \n  https://github.com/codingholt/CoinGecko-CLI/issues \n \nERROR COIN NOT FOUND')
+    process.exit()
+  }
 }
 
 export {search}
