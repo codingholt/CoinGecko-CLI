5a9f8c0c89e79f6ded490bd380ea159c2919c969
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-04-11 16:51:57 +0200

    implemented calcinterval func but dont know if cg supports it

--- src/options/chart.js
+++ src/options/chart.js
@@ -6,30 +6,42 @@ import { client } from "../client/index.js";
 
 async function makeChart(coin, days){
 
-    // function calcInterval(days){
-    //     const datapoints = 150
-    //     const maxDay =  datapoints % days
-    //     console.log(maxDay)
-    //     return maxDay
+    function calcInterval(days){
+        const datapoints = 150
+    /*    
+        something * days =  150
 
-    // }
+        3*2 =6
+
+
+        convert to min 
+    */
+        const hours = 24 * days
+        const interval = datapoints/hours
+
+        if(interval < 1){
+            console.log( `${interval*60}m`)
+        }
+        console.log(`${interval}h`)
+        return `${interval}h`
+
+    }
     
     const coingeckoREQ = await client.coinIdMarketChart({
 
         id: coin,
         vs_currency: 'usd',
         days: days,
-        // interval: calcInterval(days)
+        interval: '15min'
     })
-    let prices = coingeckoREQ['prices']
-
+    const prices = coingeckoREQ['prices']
+    console.log(prices.length)
     let priceArr = [];
     prices.forEach(i => (priceArr.push(i[1])))
 
- 
-   const sliced = priceArr.slice(-150)
+
    console.log(`${coin} chart last 150 datapoints`)
-   console.log((await acsiichart).plot(sliced, { height: 15}))
+   console.log((await acsiichart).plot(priceArr, { height: 15}))
  
 
 }
