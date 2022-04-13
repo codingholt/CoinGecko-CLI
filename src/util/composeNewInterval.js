5eb580c0d567a045faba11a3a5f8f83a2b59456d
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-04-13 10:35:06 +0200

    always include last datapoint

--- src/util/composeNewInterval.js
+++ src/util/composeNewInterval.js
@@ -4,6 +4,12 @@ function composeNewInterval(arr, nth){
     for(let i = 0; i < arr.length; i+=nth){
         result.push(arr[i])
     }
+
+    if(arr[arr.length-1] !== result[result.length-1]){
+        result.push(arr[arr.length-1])
+    }
+
+    
     return result;
 }
 
