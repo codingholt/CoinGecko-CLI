ab1e910cee64321b7b92be619ff1872aecb5cabf
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-04-13 14:32:28 +0200

    changed toml to yaml

--- src/util/getDefaults.js
+++ src/util/getDefaults.js
@@ -1,8 +1,24 @@
 import fs from 'fs';
-import toml from 'toml'
+import YAML from 'js-yaml';
 
-const data =  toml.parse(fs.readFileSync('./src/util/config.toml'));
-const default_vs_currency = data.vs_currency.default
-const default_symbol = data.vs_currency.default_symbol
+// Get YAML or throw exception on error
+
+
+  // Load the YAML
+const raw = fs.readFileSync("./src/util/config.yaml");
+const data = YAML.load(raw);
+
+
+
+const default_vs_currency = data.vs_currency.default;
+const default_symbol = data.vs_currency.default_symbol;
 
 export {default_vs_currency, default_symbol}
+
+
+//  // Saved the YAML
+// const yaml = YAML.dump(data);
+// fs.writeFileSync("./src/util/config.yaml", yaml, function (err, file) {
+//     if (err) throw err;
+//     console.log("Saved!");
+// });
