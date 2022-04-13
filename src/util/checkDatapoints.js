7d06edaf2868533f42e7cae0b080ff0e74dba4b3
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-04-13 14:40:24 +0200

    added config option max chart_datapoints

--- src/util/checkDatapoints.js
+++ src/util/checkDatapoints.js
@@ -1,9 +1,10 @@
 import {composeNewInterval} from './composeNewInterval.js';
+import {chart_dataponts} from './getDefaults.js'
 
 async function checkDatapoints(priceArr){
     const length = priceArr.length
 //if the array is longer than 150 datapoints, make it exactly or less than 150 datapoints
-    if(length > 151){
+    if(length > chart_dataponts){
         const interval = await Math.ceil(length/150)
         return composeNewInterval(priceArr, interval)
     }
--- src/util/getDefaults.js
+++ src/util/getDefaults.js
@@ -1,19 +1,19 @@
 import fs from 'fs';
 import YAML from 'js-yaml';
 
-// Get YAML or throw exception on error
-
-
-  // Load the YAML
+// Load the YAML
 const raw = fs.readFileSync("./src/util/config.yaml");
 const data = YAML.load(raw);
 
 
-
+//set constants from config
 const default_vs_currency = data.vs_currency.default;
 const default_symbol = data.vs_currency.default_symbol;
+const chart_dataponts = data.chart.max_datapoints;
 
-export {default_vs_currency, default_symbol}
+
+//export constants
+export {default_vs_currency, default_symbol, chart_dataponts}
 
 
 //  // Saved the YAML
