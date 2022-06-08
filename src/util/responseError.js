21b8e91d052d60ad5309ecf54fd92cd616a6495d
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-06-08 12:14:15 +0200

    remove unused import

--- src/util/responseError.js
+++ src/util/responseError.js
@@ -1,5 +1,3 @@
-import { search } from './search.js'
-
 function checkErr(response){
     if(response.hasOwnProperty('error')) {
         console.error(`Coingecko returns error: \n\n ${response['error']}`)
