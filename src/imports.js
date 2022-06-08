18b3e2d98a240c53a1fa99396ac22834357a3eff
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-06-08 13:30:29 +0200

    restructure imports

---
+++ src/imports.js
@@ -0,0 +1,31 @@
+import { tickers } from './options/tickers.js';
+import { fdv } from './options/fdv.js';
+import { marketcap } from './options/marketcap.js';
+import { supply } from './options/supply.js';
+import { dev } from './options/dev.js';
+import { marketcaptofdv } from './options/marketcaptoFdv.js'
+import { trending } from './options/trending.js';
+import { sentiment } from './options/sentiment.js';
+import { description } from './options/description.js';
+import { makeChart } from './options/chart.js'
+import { topMC } from './options/topMC.js';
+import { makeChartMC } from './options/chartMC.js'
+import { search } from './util/search.js';
+import { coinoverview } from './options/coinoverview.js';
+export {
+    tickers,
+    fdv,
+    marketcap,
+    supply ,
+    dev ,
+    marketcaptofdv ,
+    trending ,
+    trending ,
+    sentiment,
+    description,
+    makeChart,
+    topMC,
+    makeChartMC,
+    search,
+    coinoverview
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 #! /usr/bin/env node
 import { Command } from 'commander';
-import { search } from './util/search.js';
-import { simplePrice } from './simplePrice.js';
-import { tickers } from './options/tickers.js';
-import { fdv } from './options/fdv.js';
-import { marketcap } from './options/marketcap.js';
-import { supply } from './options/supply.js';
-import { dev } from './options/dev.js';
-import { marketcaptofdv } from './options/marketcaptoFdv.js'
-import { trending } from './options/trending.js';
-import { sentiment } from './options/sentiment.js';
-import { description } from './options/description.js';
-import { makeChart } from './options/chart.js'
-import { topMC } from './options/topMC.js';
-import { makeChartMC } from './options/chartMC.js'
-
+import{ tickers,
+    fdv,
+    marketcap,
+    supply ,
+    dev ,
+    marketcaptofdv ,
+    trending ,
+    trending ,
+    sentiment,
+    description,
+    makeChart,
+    topMC,
+    makeChartMC,
+    search,
+    coinoverview
+} from './imports.js'
 const program = new Command();
 
 
